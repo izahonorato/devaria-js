42ceeae4c75428215c4b49333719e42359f77473
izahonorato <izahonorato@gmail.com>
2021-04-28 17:43:25 -0300

    Dados e argumentos JS

---
+++ tipos-dados.js
@@ -0,0 +1,18 @@
+const  inteiro = 10;
+const decimal = 10.5;
+const bool = false;
+const nome = "Douglas";
+let copo;
+let pessoa = {
+    nome: "João"
+}
+
+const simbolo = Symbol();
+
+console.log("Tipo da variável inteiro: ", typeof inteiro);
+console.log("Tipo da variável decimal: ",typeof decimal);
+console.log("Tipo da variável bool: ",typeof bool);
+console.log("Tipo da variável nome: ",typeof nome);
+console.log("Tipo da variável copo: ",typeof copo);
+console.log("Tipo da variável pessoa: ",typeof pessoa);
+console.log("Tipo da variável simbolo: ",typeof simbolo);
