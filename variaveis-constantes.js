62d7273e09d2b6028f6cd23b9ea5aa0eb330e21c
izahonorato <izahonorato@gmail.com>
2021-04-28 17:00:51 -0300

    js básico
    primeiros programinhas em js

---
+++ variaveis-constantes.js
@@ -0,0 +1,4 @@
+var idade = 23; //variavel global
+let nome = "Wilson"; // variavel local, disponivel somente dentro do escopo local
+const empresa = "Devaria";//precisa ter valor e o mesmo nao muda
+console.log(nome, idade, empresa);
