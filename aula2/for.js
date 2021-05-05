acd22bf189239b2a0d5a8f9e6a7e76ce0e8a780a
izahonorato <izahonorato@gmail.com>
2021-05-05 18:09:25 -0300

    aula 2 da devaria

---
+++ aula2/for.js
@@ -0,0 +1,6 @@
+const trocarpneu = (pneu) => {
+    console.log('trocando pneu ' + pneu);
+}
+for (let pneuatual = 1; pneuatual <= 4; pneuatual++){
+    trocarpneu(pneuatual);
+}
---
+++ aula2/while.js
@@ -0,0 +1,16 @@
+let numeroCarro = 1;
+const lavarCarro = () => {
+    console.log(`lavando carro ${numeroCarro}`);
+}
+
+const verificarTemCarroNaFila = () => {
+    return (numeroCarro < 10);
+}
+
+let temCarroNaFila = true;
+
+while(temCarroNaFila) {
+    lavarCarro(numeroCarro);
+    temCarroNaFila = verificarTemCarroNaFila();
+    numeroCarro++;
+}
